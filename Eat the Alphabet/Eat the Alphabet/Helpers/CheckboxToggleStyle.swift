6f55b7a996f3310259682b130f752e432a4ca67d
RichardBrowning <46521212+RichardBrowning@users.noreply.github.com>
2025-06-14 23:11:12 +0800

    additional style change to toggle/checkbox

--- Eat the Alphabet/Eat the Alphabet/Helpers/CheckboxToggleStyle.swift
+++ Eat the Alphabet/Eat the Alphabet/Helpers/CheckboxToggleStyle.swift
@@ -11,12 +11,28 @@ import SwiftUI
 struct CheckboxToggleStyle: ToggleStyle {
     func makeBody(configuration: Configuration) -> some View {
         Button(action: { configuration.isOn.toggle() }) {
-            HStack {
-                Image(systemName: configuration.isOn ? "checkmark.square" : "square")
-                    .foregroundColor(configuration.isOn ? .accentColor : .gray)
+            HStack(spacing: 12) {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 6)
+                        .stroke(Color.gray.opacity(0.5), lineWidth: 1)
+                        .background(
+                            configuration.isOn ?
+                                RoundedRectangle(cornerRadius: 6).fill(Color.blue) :
+                                RoundedRectangle(cornerRadius: 6).fill(Color.white)
+                        )
+                        .frame(width: 24, height: 24)
+                    
+                    if configuration.isOn {
+                        Image(systemName: "checkmark")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 15, height: 15)
+                            .foregroundColor(.white)
+                    }
+                }
                 configuration.label
             }
+            .buttonStyle(PlainButtonStyle())
         }
-        .buttonStyle(PlainButtonStyle())
     }
 }
